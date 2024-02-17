d1812e149167bd1074102013af49366a35c913fc
AnkitaDes <desai_ankita@outlook.com>
2024-02-17 08:42:05 -0500

    add migration tables and seeds

---
+++ migrations/20240217011316_01_users_memories.js
@@ -0,0 +1,50 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", (table) => {
+      table.increments("id").primary();
+      table.string("username").notNullable();
+      table.string("email").notNullable().unique();
+      table.string("password_hash").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table
+        .timestamp("updated_at")
+        .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+    })
+    .createTable("memories", (table) => {
+      table.increments("id").unsigned().notNullable().primary();
+      table.string("description", 1000).notNullable();
+      table.string("image").notNullable();
+      table.integer("likes").notNullable().defaultTo(0);
+      table.integer("users_id").unsigned();
+      table
+        .foreign("users_id")
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.timestamp("updated_at").defaultTo(knex.fn.now());
+    })
+    .createTable("comments", (table) => {
+      table.increments("id").unsigned().notNullable().primary();
+      table.string("name").notNullable();
+      table.string("comment", 1000).notNullable();
+      table.integer("likes").notNullable().defaultTo(0);
+
+      table.integer("memories_id").unsigned();
+      table
+        .foreign("memories_id")
+        .references("id")
+        .inTable("memories")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      table.timestamp("timestamp").defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTable("comments")
+    .dropTable("memories")
+    .dropTable("users");
+};
---
+++ seeds/users-memories-comments.js
@@ -0,0 +1,120 @@
+const bcrypt = require("bcrypt");
+
+exports.seed = async function (knex) {
+  await knex("comments").del();
+  await knex("memories").del();
+  await knex("users").del();
+
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash("password", saltRounds);
+
+  await knex("users").insert([
+    {
+      id: 1,
+      username: "user1",
+      email: "user1@example.com",
+      password_hash: hashedPassword,
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now(),
+    },
+    {
+      id: 2,
+      username: "user2",
+      email: "user2@example.com",
+      password_hash: hashedPassword,
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now(),
+    },
+    {
+      id: 3,
+      username: "user3",
+      email: "user3@example.com",
+      password_hash: hashedPassword,
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now(),
+    },
+    {
+      id: 4,
+      username: "user4",
+      email: "user4@example.com",
+      password_hash: hashedPassword,
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now(),
+    },
+  ]);
+
+  await knex("memories").insert([
+    {
+      id: 1,
+      description: "This is a cute baby smiling",
+      image:
+        "https://images.unsplash.com/photo-1650023932630-0c249bbf2e96?q=80&w=1943&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      likes: 10,
+      users_id: 1,
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now(),
+    },
+    {
+      id: 2,
+      description: "This is a cute baby laughing",
+      image: "baby2.jpg",
+      likes: 20,
+      users_id: 1,
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now(),
+    },
+    {
+      id: 3,
+      description: "This is a cute baby sleeping",
+      image: "baby3.jpg",
+      likes: 30,
+      users_id: 2,
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now(),
+    },
+    {
+      id: 4,
+      description: "This is a cute baby playing",
+      image: "baby4.jpg",
+      likes: 40,
+      users_id: 2,
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now(),
+    },
+  ]);
+
+  await knex("comments").insert([
+    {
+      id: 1,
+      name: "User 1",
+      comment: "This is a comment 1",
+      likes: 10,
+      memories_id: 1,
+      timestamp: knex.fn.now(),
+    },
+    {
+      id: 2,
+      name: "User 2",
+      comment: "This is a comment 2",
+      likes: 20,
+      memories_id: 1,
+      timestamp: knex.fn.now(),
+    },
+    {
+      id: 3,
+      name: "User 3",
+      comment: "This is a comment 3",
+      likes: 30,
+      memories_id: 2,
+      timestamp: knex.fn.now(),
+    },
+    {
+      id: 4,
+      name: "User 4",
+      comment: "This is a comment 4",
+      likes: 40,
+      memories_id: 2,
+      timestamp: knex.fn.now(),
+    },
+  ]);
+};
