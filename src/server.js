41a37a53a1c9daa1775afaabd1af01fe21d2620e
AnkitaDes <desai_ankita@outlook.com>
2024-02-19 12:42:30 -0500

    Add environment variables and cloudinary package

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
-require("dotenv").config();
-const express = require("express");
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+// import memoryRoutes from "./routes/memory-routes.js";
+
+dotenv.config({ path: "../.env" });
+
 const app = express();
-const cors = require("cors");
-const memoryRoutes = require("./src/routes/memory-routes");
 const PORT = process.env.PORT || 5050;
 
 app.use(cors());
@@ -10,7 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // basic home route
-app.use("/memories", memoryRoutes);
+// app.use("/memories", memoryRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
